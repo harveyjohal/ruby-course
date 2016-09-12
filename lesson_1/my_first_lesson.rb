0cc0bb3176a7c162e359d4f8c7033f078a348eb2
Harvey Johal <harvey.johal@onfido.com>
2016-09-12 20:03:25 +0100

    lesson 1 rename folder

---
+++ lesson_1/my_first_lesson.rb
@@ -0,0 +1,4 @@
+# This is my first ruby output
+puts "Hello! This is my first line of code!" # This is an inline comment
+puts "I enjoy Ruby!"
+puts "What the F"
---
+++ lesson_1/my_new_ruby_file.rb
@@ -0,0 +1,9 @@
+puts "Hello, World"
+
+=begin
+
+print "My name is Harvey\n"
+
+=end
+
+print "Just some text here!"
