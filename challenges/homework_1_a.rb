14b6d88f204537a241a62ef1fc38b4bd8dbb5e99
Harvey Johal <harvey.johal@onfido.com>
2016-09-12 13:59:47 +0100

    Fixed spelling mistake

--- challenges/homework_1_a.rb
+++ challenges/homework_1_a.rb
@@ -2,7 +2,7 @@ def square_root(number)
   puts Math.sqrt(number)
 end
 
-print "Which number do you want to find out the sqaure root of? "
+print "Which number do you want to find out the square root of? "
 number = gets.to_i
 
 puts square_root(number)
